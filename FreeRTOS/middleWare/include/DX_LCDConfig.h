851719c1dba808a569425393ee474228d49a4f41
RockyTieGu <1530961499@qq.com>
2020-03-09 21:29:42 +0800

    1.This is C035QC05N(320240_RGB565_V0.1) program.

--- FreeRTOS/middleWare/include/DX_LCDConfig.h
+++ FreeRTOS/middleWare/include/DX_LCDConfig.h
@@ -2,8 +2,8 @@
 #define _DX_LCDConfig_H_
 #include "wbtypes.h"
 
-#define	_LCD_WIDTH				(800)
-#define	_LCD_HEIGHT				(480)
+#define	_LCD_WIDTH				(320)
+#define	_LCD_HEIGHT				(240)
 
 //System  hardware version
 #define CENTRY_LCD_HW_VERSION	"V2.0"
@@ -12,7 +12,7 @@
 #define CENTRY_LCD_SW_VERSION	"V0.1"
 
 //Current LCD type name
-#define CENTRY_LCD_TYPE			"C043WVH02NE"
+#define CENTRY_LCD_TYPE			"C035QC05N"
 
 //Set lcd color bits ,16BIT 18BITS .defautlt lcd color is 16 bits
 #define LCD_BITS_MODE			(16)
@@ -26,7 +26,7 @@
 
 /*To drive the C035QC05N lcd ,the DE pin must be forced to GND */
 // lcd clock frequency[1~120]MHZ
-#define LCD_CLOCK_FREQUENCY			(20)
+#define LCD_CLOCK_FREQUENCY			(7)
 
 //Specify the number of pixel clock in each line
 #define LCD_PIXEL_PER_LINE			(_LCD_WIDTH)
@@ -37,26 +37,6 @@
 //Specify the number of pixel in each line or row of screen
 #define LCD_PIXEL_PER_LINE			(_LCD_WIDTH)
 
-
-#if 1
-// Horizontal sync pulse width value: [0~255]
-#define LCD_RGB_HPW 				(2)
-
-// Horizontal back porch value: [0~255]
-#define LCD_RGB_HBP 				(0xa0) 
-
-// Horizontal front porch value: [0~255]
-#define LCD_RGB_HFP 				(0xa2)
-
-// Vertical sync pulse width value: [0~255]
-#define LCD_RGB_VPW 				(2)
-
-// Vertical back porch value: [0~255]
-#define LCD_RGB_VBW 				(0x18)
-
-// Vertical front porch width value: [0~255]
-#define LCD_RGB_VFW 				(0x16) 
-#else
 // Horizontal sync pulse width value: [0~255]
 #define LCD_RGB_HPW 				(50)
 
@@ -74,19 +54,18 @@
 
 // Vertical front porch width value: [0~255]
 #define LCD_RGB_VFW 				(16) 
-#endif
 
 // Vsync polarity (TRUE  or FALSE)
-#define LCD_RGB_VSYNC_POL	 		(FALSE)
+#define LCD_RGB_VSYNC_POL	 		(TRUE)
 
 // Hsync polarity (TRUE  or FALSE)
-#define LCD_RGB_HSYNC_POL	 		(FALSE)
+#define LCD_RGB_HSYNC_POL	 		(TRUE)
 
 // VDEN polarity  (TRUE  or FALSE)
 #define LCD_RGB_DE_POL				(TRUE)
 
 // CLOCK polarity (TRUE  or FALSE)
-#define LCD_RGB_CLOCK_POL			(FALSE)
+#define LCD_RGB_CLOCK_POL			(TRUE)
 
 
 #endif
